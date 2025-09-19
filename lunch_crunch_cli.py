fe9b2a9b0dd7d56a0c2ec33d660714b467bb4ed3
Fabian Sauer <fabian.sauer@proton.me>
2025-09-19 02:00:36 +0200

    Fix CLI version for missing config file

--- lunch_crunch_cli.py
+++ lunch_crunch_cli.py
@@ -87,8 +87,6 @@ def main():
     config_file = config_path / 'config.toml'
 
     mailer = OrderMailer(config_file)
-    mailer.save_config()
-    mailer.save_template()
 
     place_order(mailer)
     print_preview(mailer)
